6462fa3f67d8c8d19f8d4634db882c10adb46200
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-02-25 14:40:49 +0900

    FIX: logger 오류 수정

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -3,16 +3,12 @@ import logging
 
 def init_logger(name:str, level:str) -> logging:
     logger = logging.getLogger(name=name)
-    logger.setLevel(level)
+    logger.setLevel(level=level)
 
-    formatter = logging.Formatter('|%(asctime)s|==|%(levelname)s| %(funcName)s | %(message)s',
-                                datefmt='%Y-%m-%d %H:%M:%S'
-                                )
-
-    stream_handler = logging.StreamHandler() ## 스트림 핸들러 생성
-    stream_handler.setFormatter(formatter) ## 텍스트 포맷 설정
+    formatter = logging.Formatter("| %(levelname)s | %(funcName)s | %(message)s")
     
-    logger.addHandler(stream_handler) ## 핸들러 등록
-
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
 
     return logger
