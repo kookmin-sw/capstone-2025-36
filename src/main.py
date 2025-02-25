d88ccd2beecb8978ebf4d9e93da8d827d3bf2772
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-02-25 15:37:26 +0900

    FEAT: table html에서 table 추출 함수 추가

--- src/main.py
+++ src/main.py
@@ -1,21 +1,21 @@
 import os
+from pathlib import Path
 from pyhwpx import Hwp
-from utils.file_handler import get_filenames_with_type
+from parsers.hwp_to_html import _extract_html_table
+from parsers.table_parser import get_txt_from_table
 
 # DIR PATHS
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 DATA_DIR = os.path.join(ROOT_DIR, "assets", "input")
 OUTPUT_DIR = os.path.join(ROOT_DIR, "assets", "output")
 
-TEST_HWP_PATH = os.path.join(DATA_DIR, "test", "test.hwp")
+TEST_DIR = os.path.join(DATA_DIR, "test")
 
 
 def main():
-    hwp_file_paths = get_filenames_with_type(DATA_DIR, "hwp")
-    print(hwp_file_paths)
-    # hwp = Hwp()
-    # hwp.open(TEST_HWP_PATH)
-
-
+    hwp = Hwp()
+    table_ls, mean_time = _extract_html_table(hwp_dir_path=Path(TEST_DIR), hwp=hwp)
+    get_txt_from_table(table_ls[0])
+    
 if __name__ == "__main__":
     main()
