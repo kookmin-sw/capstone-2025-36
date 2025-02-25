bc07f3d219a31470cb076a774435cbbe3ff0ffc0
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-02-25 14:43:50 +0900

    FEAT: logger에 timestamp 추가

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -5,7 +5,9 @@ def init_logger(name:str, level:str) -> logging:
     logger = logging.getLogger(name=name)
     logger.setLevel(level=level)
 
-    formatter = logging.Formatter("| %(levelname)s | %(funcName)s | %(message)s")
+    formatter = logging.Formatter('|%(asctime)s|==|%(levelname)s| %(funcName)s | %(message)s',
+                                datefmt='%Y-%m-%d %H:%M:%S'
+                                )
     
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
