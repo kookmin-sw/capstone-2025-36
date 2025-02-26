2c87130cc1f91f4014b979f91d3fb6d0ed13e6ae
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-02-26 18:00:30 +0900

    FEAT: test 용 main 수정

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import os
 from pathlib import Path
-from pyhwpx import Hwp
-from parsers.hwp_to_html import extract_html_from_hwp
 from parsers.table_parser import get_json_from_table
 from parsers.image_ocr import convert_image_to_json
 from utils.file_handler import get_table_from_pickling
+
+if os.name == "nt":
+    from pyhwpx import Hwp
+    from parsers.hwp_to_html import extract_html_from_hwp
 
 # DIR PATHS
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -14,19 +16,20 @@ OUTPUT_DIR = os.path.join(ROOT_DIR, "assets", "output")
 TEST_DIR = os.path.join(DATA_DIR, "test")
 TEST_HWP_PATH = os.path.join(TEST_DIR, "test1.hwp")
 TEST_PICKLE_PATH = os.path.join(TEST_DIR, "test1.pickle")
-TEST_IMG_PATH = "C:/Users/doosa/project/capstone-data/assets/output/test/test1_1.jpg"
+TEST_IMG_MATH_PATH = os.path.join(TEST_DIR, "test1_2.jpg")
+TEST_IMG_PATH = os.path.join(TEST_DIR, "test4_32.jpg")
 
 def main():
-    hwp = Hwp()
-    table_ls, img_ls = extract_html_from_hwp(hwp_dir_path=Path(TEST_DIR), output_dir_path=Path(OUTPUT_DIR), hwp=hwp)
+    # hwp = Hwp()
+    # table_ls, img_ls = extract_html_from_hwp(hwp_dir_path=Path(TEST_DIR), output_dir_path=Path(OUTPUT_DIR), hwp=hwp)
     
     #TODO TEST용 코드
-    # table_data = get_table_from_pickling(TEST_PICKLE_PATH)
-    # for table in table_ls:
-    #     print(get_json_from_table(table))
+    table_ls = get_table_from_pickling(TEST_PICKLE_PATH)
+    print(get_json_from_table(table_ls[0]))
+    print(convert_image_to_json([Path(TEST_IMG_PATH)]))
+
     # for img_path in img_ls:
     #     print(str(img_path))
-    print(convert_image_to_json(img_ls))
     
 if __name__ == "__main__":
     main()
