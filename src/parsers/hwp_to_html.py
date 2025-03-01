f4f73b584a0d2e859cef193861875dbea4717e57
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-03-02 00:38:49 +0900

    fix(이미지 처리): 이미지 경로가 none일 때도 실행 가능하도록 수정

--- src/parsers/hwp_to_html.py
+++ src/parsers/hwp_to_html.py
@@ -1,4 +1,3 @@
-import os
 import io
 import pickle
 import shutil
@@ -17,6 +16,79 @@ from parsers.table_parser import Table
 
 
 logger = init_logger(__file__, "DEBUG")
+
+
+def extract_html_from_hwp(hwp_dir_path: Path, output_dir_path: Path, hwp: Hwp) -> Tuple[List[Table], List[str]]:
+    table_ls = list()
+    img_ls = list()
+
+    if not output_dir_path.exists():
+        output_dir_path.mkdir(parents=True)
+
+    for hwp_file_path in hwp_dir_path.rglob("*.hwp"):
+        output_hwp_path = output_dir_path / hwp_file_path.parent.stem
+
+        if not output_hwp_path.exists():
+            output_hwp_path.mkdir(parents=True)
+
+        logger.info(f"Hwp {hwp_file_path} loding..")
+        hwp.open(hwp_file_path.as_posix())
+
+        one_file_table_ls = list()
+
+        ctrl = hwp.HeadCtrl
+        while ctrl:
+            if ctrl.UserDesc == "표" or ctrl.UserDesc == "그림":
+                hwp.SetPosBySet(ctrl.GetAnchorPos(0))
+                hwp.HAction.Run("SelectCtrlFront")
+                hwp.HAction.Run("Copy")
+
+                if ctrl.UserDesc == "표":
+                    try:
+                        html = _get_tag_from_clipboard(tag="table")
+                        hwp.ShapeObjTableSelCell()
+                        table_df = pd.read_html(io.StringIO(html))[0]
+                        row_num, col_num = table_df.shape
+                        
+                    except BaseException as e:
+                        logger.error(f"TableExtractionError: Failed to extract table: {e}")
+                        ctrl = ctrl.Next
+                        continue
+
+                    if not row_num or not col_num:
+                        ctrl = ctrl.Next
+                        continue
+
+                    table = Table(html=html, col=col_num, row=row_num)
+
+                    one_file_table_ls.append(table)
+                elif ctrl.UserDesc == "그림":
+                    try:
+                        img_src = _get_tag_from_clipboard(tag="img")
+                        if not img_src:
+                            ctrl = ctrl.Next
+                            continue
+                        img_save_path = output_hwp_path / f"{hwp_file_path.stem}_{len(img_ls)+1}.jpg"
+                        shutil.copy(img_src, img_save_path)
+                        img_ls.append(img_save_path)
+
+                    except BaseException as e:
+                        logger.error(f"ImageExtractionError: Failed to extract image: {e}")
+                        ctrl = ctrl.Next
+                        continue
+
+                ctrl = ctrl.Next
+
+            else:
+                ctrl = ctrl.Next
+                continue
+
+        table_ls.extend(one_file_table_ls)
+
+        pickle_save_path = output_hwp_path / f"{hwp_file_path.stem}.pickle"
+        pickle_save_path.write_bytes(pickle.dumps(one_file_table_ls))
+
+    return table_ls, img_ls
 
 
 def _get_tag_from_clipboard(tag: str, max_retries: int = 10) -> Optional[str]:
@@ -63,70 +135,6 @@ def _get_tag_from_clipboard(tag: str, max_retries: int = 10) -> Optional[str]:
     return None
 
 
-def extract_html_from_hwp(hwp_dir_path: Path, output_dir_path: Path, hwp: Hwp) -> Tuple[List[Table], List[str]]:
-    table_ls = list()
-    img_ls = list()
-
-    output_dir_path = Path(output_dir_path) / hwp_dir_path.stem
-    if not output_dir_path.exists():
-        output_dir_path.mkdir(parents=True)
-
-    for hwp_file_path in hwp_dir_path.glob("*.hwp"):
-        logger.info(f"Hwp {hwp_file_path} loding..")
-        hwp.open(hwp_file_path.as_posix())
-
-        one_file_table_ls = list()
-
-        ctrl = hwp.HeadCtrl
-        while ctrl:
-            if ctrl.UserDesc == "표" or ctrl.UserDesc == "그림":
-                hwp.SetPosBySet(ctrl.GetAnchorPos(0))
-                hwp.HAction.Run("SelectCtrlFront")
-                hwp.HAction.Run("Copy")
-
-                if ctrl.UserDesc == "표":
-                    try:
-                        html = _get_tag_from_clipboard(tag="table")
-                        hwp.ShapeObjTableSelCell()
-                        table_df = pd.read_html(io.StringIO(html))[0]
-                        row_num, col_num = table_df.shape
-                        
-                    except BaseException as e:
-                        logger.error(f"TableExtractionError: Failed to extract table: {e}")
-                        ctrl = ctrl.Next
-                        continue
-
-                    if not row_num or not col_num:
-                        ctrl = ctrl.Next
-                        continue
-
-                    table = Table(html=html, col=col_num, row=row_num)
-
-                    one_file_table_ls.append(table)
-                elif ctrl.UserDesc == "그림":
-                    try:
-                        img_src = _get_tag_from_clipboard(tag="img")
-                        img_save_path = output_dir_path / f"{hwp_file_path.stem}_{len(img_ls)+1}.jpg"
-                        shutil.copy(img_src, img_save_path)
-                        img_ls.append(img_save_path)
-
-                    except BaseException as e:
-                        logger.error(f"ImageExtractionError: Failed to extract image: {e}")
-                        ctrl = ctrl.Next
-                        continue
-
-                ctrl = ctrl.Next
-
-            else:
-                ctrl = ctrl.Next
-                continue
-
-        table_ls.extend(one_file_table_ls)
-
-        pickle_save_path = output_dir_path / f"{hwp_file_path.stem}.pickle"
-        pickle_save_path.write_bytes(pickle.dumps(one_file_table_ls))
-
-    return table_ls, img_ls
 
 # 추가로 만든 hwp2thtml 함수
 def _convert_hwp_to_html(hwp_path: str, output_path: str) -> None:
