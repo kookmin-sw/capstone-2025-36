68c38cc3d40f01538802a674913c0a453dea052a
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-03-06 22:13:28 +0900

    feat (상수): main의 일반적인 상수값들 옮김

---
+++ src/utils/constants.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+# DIR PATHS
+ROOT_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = ROOT_DIR / "assets" / "test"
+OUTPUT_DIR = ROOT_DIR / "assets" / "output"
+OUTPUT_JSON = OUTPUT_DIR / "output.json"
+
+# 추출할 Ctrl obj
+CTRL_TYPES = ["표", "그림", "수식"]
