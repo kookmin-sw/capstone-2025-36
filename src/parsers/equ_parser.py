fce1364f23f790daa4e46582b0130edeae479c6d
차민수 <66284303+Blue-Ladder@users.noreply.github.com>
2025-03-10 04:11:31 +0900

    REFACT: 변환 매핑 constants.py에 저장

--- src/parsers/equ_parser.py
+++ src/parsers/equ_parser.py
@@ -2,6 +2,7 @@ import re
 from typing import List
 from pyhwpx import Hwp
 from py_asciimath.translator.translator import MathML2Tex
+from constants import UNICODE_LATEX_MAP, LATEX_UNICODE_MAP
 from utils.logger import init_logger
 from utils.window_asciimath import modify_init_py
 
@@ -26,29 +27,6 @@ def extract_latex(hwp: Hwp) -> List[str] :
     hwp.clear() # 한글 파일을 닫는 함수
     return split_latex(combined_latex)
 
-
-# 유니코드 폰트 문자 → LaTeX 변환 맵
-UNICODE_LATEX_MAP = {
-    "ℤ": r"\mathbb{Z}",
-    "ℝ": r"\mathbb{R}",
-    "ℕ": r"\mathbb{N}",
-    "ℍ": r"\mathbb{H}",
-    "ℚ": r"\mathbb{Q}",
-    "ℂ": r"\mathbb{C}",
-    "ℙ": r"\mathbb{P}",
-    "not=": r"\neq"
-}
-
-# LaTex 문자 → 유니코드 폰트 변환맵
-LATEX_UNICODE_MAP = {
-    "\\text{mathbbZ}" : "ℤ",
-    "\\text{mathbbR}" : "ℝ",
-    "\\text{mathbbN}" : "ℕ",
-    "\\text{mathbbQ}" : "ℚ",
-    "\\text{mathbbC}" : "ℂ",
-    "\\text{mathbbH}" : "ℍ",
-    "\\text{mathbbP}" : "ℙ"
-}
 
 def unicode_to_latex(text: str) -> str:
     
