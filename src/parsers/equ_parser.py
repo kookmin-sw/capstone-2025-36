3c19700b4548cc5e073f7bb4345b07947e160349
차민수 <66284303+Blue-Ladder@users.noreply.github.com>
2025-03-10 04:57:45 +0900

    REFACT: 함수명 맟 변수 몇 변경

--- src/parsers/equ_parser.py
+++ src/parsers/equ_parser.py
@@ -9,7 +9,7 @@ from utils.window_asciimath import modify_init_py
 modify_init_py()
 logger = init_logger(__file__, "DEBUG")
 
-def extract_latex(hwp: Hwp) -> List[str] :
+def extract_latex_list(hwp: Hwp) -> List[str] :
     
     """
     hwp에서 latex 수식 리스트를 추출하는 함수
@@ -20,21 +20,21 @@ def extract_latex(hwp: Hwp) -> List[str] :
     Returns:
         List[str]:LaTex 수식 문자열 리스트
     """
-    eq_list = get_eq_list(hwp)
-    combined_eq = join_hwp_eq(eq_list)
-    pure_latex = [unicode_to_latex(text) for text in combined_eq]
-    combined_latex = parse_mathml_to_latex(pure_latex, hwp)
+    eq_list = _get_eq_list(hwp)
+    combined_eq_list = _join_hwp_eq(eq_list)
+    pure_latex = [_unicode_to_latex(eq) for eq in combined_eq_list]
+    combined_latex = _parse_mathml_to_latex(pure_latex, hwp)
     hwp.clear() # 한글 파일을 닫는 함수
-    return split_latex(combined_latex)
+    return _split_latex(combined_latex)
 
 
-def unicode_to_latex(text: str) -> str:
+def _unicode_to_latex(eq_text: str) -> str:
     
     """
     문자열에서 유니코드 수학 기호를 LaTeX 코드로 변환
     
     Args:
-        text(str): 유니코드 문자와 not equal 문자가 포함되어 있는 hwp 수식 문자열
+        eq_text(str): 유니코드 문자와 not equal 문자가 포함되어 있는 hwp 수식 문자열
 
     Returns:
         str: 유니코드 문자와 not equal 문자를 미리 latex 형식으로 변환한 hwp 수식 문자열
@@ -44,17 +44,17 @@ def unicode_to_latex(text: str) -> str:
     def replace_func(match):
         return UNICODE_LATEX_MAP[match.group(0)]
 
-    return pattern.sub(replace_func, text)
+    return pattern.sub(replace_func, eq_text)
 
 
-def latex_to_unicode(text: str) -> str:
+def _latex_to_unicode(eq_text: str) -> str:
     
     """
     유니코드에서 LaTex 표현식으로 미리 변경한 수학 기호가 mathML -> LaTex로 변경 될 때,
     text로 인식 되기 때문에 text형식의 Latex를 다시 유니코드 문자로 변경하는 함수 
     
     Args:
-        text(str): Latex로 변경이 완료된 수식 문자열
+        eq_text(str): Latex로 변경이 완료된 수식 문자열
 
     Returns:
         str: 유니코드 문자를 처리한 LaTex 수식 문자열
@@ -64,9 +64,9 @@ def latex_to_unicode(text: str) -> str:
     def replace_func(match):
         return LATEX_UNICODE_MAP[match.group(0)]
 
-    return pattern.sub(replace_func, text)
+    return pattern.sub(replace_func, eq_text)
 
-def get_eq_list(hwp : Hwp) -> List[str]:
+def _get_eq_list(hwp : Hwp) -> List[str]:
     
     """
     hwp에서 수식 객체의 수식을 일괄 추출하는 함수
@@ -88,7 +88,7 @@ def get_eq_list(hwp : Hwp) -> List[str]:
 
     return eq_list
 
-def join_hwp_eq(eq_list : List[str]) -> List[str]:
+def _join_hwp_eq(eq_list : List[str]) -> List[str]:
 
     """
     추출한 수식 리스트를 긴 문자열로 합치는 함수
@@ -106,24 +106,24 @@ def join_hwp_eq(eq_list : List[str]) -> List[str]:
     
     SPLITPOINT = "#"
     MAXLENTH = 1500
-    combined_eq = []
+    combined_eq_list = []
     current = []
     
     for s in eq_list:
         # 문자열을 추가했을 때 길이가 max_length를 넘는지 확인
         combined = SPLITPOINT.join(current + [s])  # 현재 리스트에 문자열 s를 추가한 후, 구분자로 결합
         if len(combined) > MAXLENTH:
-            combined_eq.append(SPLITPOINT.join(current))  # 현재까지의 문자열을 구분자로 결합하여 저장
+            combined_eq_list.append(SPLITPOINT.join(current))  # 현재까지의 문자열을 구분자로 결합하여 저장
             current = [s]  # 새로운 문자열을 시작
         else:
             current.append(s)  # 기존 리스트에 문자열 추가
     
     if current:  # 마지막 남은 문자열 추가
-        combined_eq.append(SPLITPOINT.join(current))
+        combined_eq_list.append(SPLITPOINT.join(current))
     
-    return combined_eq
+    return combined_eq_list
 
-def parse_mathml_to_latex(combined_eq : List[str], hwp : Hwp) -> List[str]:
+def _parse_mathml_to_latex(combined_eq_list : List[str], hwp : Hwp) -> List[str]:
 
     """
     길게 합쳐진 수식 문자열 리스트를 길게 합쳐진 LaTex 리스트로 만드는 함수
@@ -136,20 +136,20 @@ def parse_mathml_to_latex(combined_eq : List[str], hwp : Hwp) -> List[str]:
 
 
     Args:
-        combined_eq(List[str]): 길게 합쳐진 수식 문자열 리스트
+        combined_eq_list(List[str]): 길게 합쳐진 수식 문자열 리스트
 
     Returns:
         List[str]: 길게 합쳐진 LaTex 리스트
     """
 
-    combined_latex =[]
+    latex_list = []
     
-    for string in combined_eq:
+    for combined_eq in combined_eq_list:
         
         hwp.MovePos(3)            #문서 끝으로 이동
         action = "EquationCreate"
         pset = hwp.HParameterSet.HEqEdit
-        pset.string = string
+        pset.string = combined_eq
         hwp.HAction.Execute(action, pset.HSet)
         ctrl = hwp.LastCtrl
 
@@ -161,14 +161,14 @@ def parse_mathml_to_latex(combined_eq : List[str], hwp : Hwp) -> List[str]:
         mathml2tex = MathML2Tex()
         with open(mml_path) as f:
             mml_eq = f.read()
-        tex_eq = mathml2tex.translate(mml_eq, network=True, from_file=False)
-        combined_latex.append(tex_eq)
+        latex_eq = mathml2tex.translate(mml_eq, network=True, from_file=False)
+        combined_latex_list.append(latex_eq)
 
-    combined_latex = [latex_to_unicode(text) for text in combined_latex]
+    combined_latex_list = [_latex_to_unicode(eq_text) for eq_text in latex_list]
 
-    return combined_latex
+    return combined_latex_list
 
-def split_latex(combined_latex : List[str]) -> List[str]:
+def _split_latex(combined_latex : List[str]) -> List[str]:
 
     """
     길게 합쳐진 LaTex 문자열 리스트를 자르는 함수
