78fbf3f4f07bef06b13778e88c3f95e577465b35
차민수 <66284303+Blue-Ladder@users.noreply.github.com>
2025-03-14 05:20:19 +0900

    FIX: 객체뒤에 마커를 남기도록 변경

--- src/parsers/process_hwp_docs.py
+++ src/parsers/process_hwp_docs.py
@@ -33,6 +33,10 @@ class HwpController:
         self.one_file_equations = []
         
         start = time.time()
+        image_cnt = 0
+        table_cnt = 0
+        equation_cnt = 0
+        
         table_cnt = 1
         image_count = 1
 
@@ -40,9 +44,14 @@ class HwpController:
         self.hwp_equation = []
         for ctrl in self.hwp.ctrl_list:
             if ctrl.UserDesc == "수식":
+                equation_cnt += 1
                 self._copy_ctrl(ctrl)
-                try: 
+                try:
                     self.hwp_equation.append(ctrl.Properties.Item('VisualString'))
+                    
+                    self.hwp.move_to_ctrl(ctrl)
+                    self.hwp.MoveRight()
+                    self.hwp.insert_text(f'{{eqation_{equation_cnt}}}')
         
                 except Exception as e:
                     logger.error(f"EqualationExtractionError: {str(e)}")
@@ -51,11 +60,16 @@ class HwpController:
 
         for ctrl in self.hwp.ctrl_list:
             if ctrl.UserDesc == "표":
+                table_cnt += 1
                 self._copy_ctrl(ctrl)
                 try:
                     html = get_table_from_clipboard()
                     table_df = pd.read_html(io.StringIO(html))[0]
                     row_num, col_num = table_df.shape
+                    
+                    self.hwp.move_to_ctrl(ctrl)
+                    self.hwp.MoveRight()
+                    self.hwp.insert_text(f'{{table_{table_cnt}}}')
                         
                 except BaseException as e:
                     logger.error(f"TableExtractionError: Failed to extract table: {e}")
@@ -63,15 +77,23 @@ class HwpController:
 
                 if not row_num or not col_num:
                     continue
+
                 
                 self.one_file_table_list[table_cnt] = html
                 table_cnt += 1
             
             elif ctrl.UserDesc == "그림":
+                image_cnt += 1
                 self._copy_ctrl(ctrl)
                 try:
                     img_tmp_path = Path(get_image_from_clipboard())
                     if not img_tmp_path:
+                        continue   
+                    self.one_file_images[str(img_tmp_path)] = ''
+                    
+                    self.hwp.move_to_ctrl(ctrl)
+                    self.hwp.MoveRight()
+                    self.hwp.insert_text(f'{{image_{image_cnt}}}')  
                         continue
                     with img_tmp_path.open("rb") as f:
                         img_data = f.read()
