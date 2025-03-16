f7923e6b4eff6760947a5f0a3c53dea4e57623cc
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-03-17 00:04:22 +0900

    REFACT: img ocr 코드 변경

--- src/parsers/image_ocr.py
+++ src/parsers/image_ocr.py
@@ -1,29 +1,75 @@
+import io
 import os
+import cv2
+import numpy as np
 import easyocr
+import pytesseract
 from pathlib import Path
-from typing import List, Dict
 from dotenv import load_dotenv
+from PIL import Image
+from transformers import pipeline
 from langchain_community.document_loaders import AzureAIDocumentIntelligenceLoader
 from utils.logger import init_logger
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+import requests
 
 
 load_dotenv()
-reader = easyocr.Reader(["en", "ko"])
 logger = init_logger(__file__, "DEBUG")
 
 
-def convert_image_to_json(img_path:Path) -> str:
-    """
-    image를 JSON으로 변환하여 리턴
-    """
-    if not img_path.exists():
-        logger.error(f"이미지 파일이 존재하지 않습니다: {img_path}")
+class ImageOCR:
+    def __init__(self) -> None:
+        self.reader = easyocr.Reader(["en", "ko"])
+        self.type_list = ["Graph", "Diagram", "Text", "Formula"]
+        self.detector = pipeline(model="openai/clip-vit-large-patch14", task="zero-shot-image-classification")
+        pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+        self.processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
+        self.vision_model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
 
-    # TODO: OCR 모델 선정
-    # ocr_text = _extract_text_from_img(str(img_path))
-    ocr_text = "".join(reader.readtext(str(img_path), detail=0))
+    def convert_img_to_txt(self, binary_image: bytes) -> str:
+        image = Image.open(io.BytesIO(binary_image))
+        image_type = self._classficate_image(image)
+        if image_type == "Text":
+            # binary_image = self._preprocess_image(binary_image)
+            ocr_text = pytesseract.image_to_string(image, lang="kor")
+            return ocr_text
+        elif image_type == "Formula":
+            return self._process_equation(image)
+    
+    def _classficate_image(self, image: Image.Image) -> str:
+        """
+        Image를 분류해주는 함수
 
-    return ocr_text
+        Args:
+
+        Returns:
+    
+        """
+        predictions = self.detector(image, candidate_labels=self.type_list)
+        pred_max = 0
+        pred_val = None
+
+        for pred in predictions:
+            if pred['score'] > pred_max:
+                pred_max = pred['score']
+                pred_val = pred['label']
+
+        return pred_val
+    
+    def _preprocess_image(self, image_bytes: bytes):
+        """이미지 전처리 (흑백 변환 + 이진화)"""
+        image = Image.open(io.BytesIO(image_bytes)).convert("L") 
+        img_array = np.array(image)
+        _, binary_img = cv2.threshold(img_array, 150, 255, cv2.THRESH_BINARY)  # 이진화
+        return Image.fromarray(binary_img)
+
+    def _process_equation(self, image: Image.Image):
+        pixel_values = self.processor(images=image.convert('RGB'), return_tensors="pt").pixel_values
+        generated_ids = self.vision_model.generate(pixel_values)
+        text = self.processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+        return text
 
 
 def _extract_text_from_img(jpg_path:str) -> str:
