55740f9dbca632372f05d2325c6dd5194ee0a71b
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-03-26 12:57:34 +0900

    FEAT: JSON을 직접 Image로 변환하는 과정 추가

--- src/app.py
+++ src/app.py
@@ -1,24 +1,42 @@
+import json
+import time
 import streamlit as st
-from PIL import Image
 
 from parsers.image_ocr import ImageOCR
+from parsers.table_parser import TableParser
 from utils.logger import init_logger
 
 
 logger = init_logger(__file__, "DEBUG")
-
-
-st.title("이미지 OCR 추출기")
-
-uploaded_file = st.file_uploader("이미지를 업로드 하세요", type=["jpg", "png", "jpeg"])
-if uploaded_file is not None:
-    file_bytes = uploaded_file.getvalue()
-    image = Image.open(uploaded_file)
-    st.image(image, caption='업로드한 이미지', use_container_width=True)
 image_ocr = ImageOCR()
 
-if st.button("텍스트 추출"):
-    image_type, image_text = image_ocr.convert_img_to_txt(file_bytes)    
+st.cache_data.clear()
+st.cache_resource.clear()
+
+uploaded_file = st.file_uploader("JSON 파일을 업로드하세요", type=["json"], key="file_uploader")
+
+if uploaded_file is not None:
+    json_data = json.load(uploaded_file)
+
+    if st.button("텍스트 추출"):
+        with st.spinner("텍스트를 추출 중입니다..."):
+            for curr_key, curr_json in json_data.items():
+                logger.info('complete json loading')
+        # table parsing
+        # table_data = curr_json['tables']
+        # table_parser = TableParser()
+
+        # for table_name in table_data.keys():
+        #     curr_json[table_name] = table_parser.parse_table_from_html(table_data[table_name])
     
-st.write("추출된 텍스트:")
-st.text_area("OCR 결과", image_text, height=200)
+
+                # image to text
+                image_data = curr_json['images']
+                logger.info('load image data')
+                
+                for image_name, image_byte in image_data.items():
+                    logger.info(image_ocr.convert_img_to_txt(image_byte))
+                    # curr_json[image_name] = 
+
+    st.write("업로드된 JSON 데이터:")
+    st.json(json_data)
