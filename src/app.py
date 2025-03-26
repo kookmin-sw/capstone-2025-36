9740ea04835ea3db607e2e24dbbe9b4daad2c204
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-03-26 15:22:13 +0900

    FEAT: resource를 한번만 선언하도록 변경

--- src/app.py
+++ src/app.py
@@ -1,17 +1,30 @@
 import json
-import time
+import asyncio
 import streamlit as st
 
 from parsers.image_ocr import ImageOCR
 from parsers.table_parser import TableParser
 from utils.logger import init_logger
 
+@st.cache_resource
+def load_resource():
+    # 한 번만 실행되는 코드
+    logger = init_logger(__file__, "DEBUG")
+    image_ocr = ImageOCR()
+    table_parser = TableParser()
+    resource = {
+        "logger": logger,
+        "image_ocr": image_ocr,
+        "table_parser": table_parser,
+    }  # 예시 리소스
+    return resource
 
-logger = init_logger(__file__, "DEBUG")
-image_ocr = ImageOCR()
 
 st.cache_data.clear()
 st.cache_resource.clear()
+
+resource = load_resource()
+
 
 uploaded_file = st.file_uploader("JSON 파일을 업로드하세요", type=["json"], key="file_uploader")
 
@@ -21,21 +34,27 @@ if uploaded_file is not None:
     if st.button("텍스트 추출"):
         with st.spinner("텍스트를 추출 중입니다..."):
             for curr_key, curr_json in json_data.items():
-                logger.info('complete json loading')
-        # table parsing
-        # table_data = curr_json['tables']
-        # table_parser = TableParser()
+                resource["logger"].info('complete json loading')
 
-        # for table_name in table_data.keys():
-        #     curr_json[table_name] = table_parser.parse_table_from_html(table_data[table_name])
+                # table parsing
+                table_data = curr_json['tables']
+                resource["logger"].info('load Table data')
+            
+                for table_name, table_html in table_data.items():
+                    json_data[curr_key]['tables'][table_name] = resource["table_parser"].parse_table_from_html(table_html)
     
-
                 # image to text
                 image_data = curr_json['images']
-                logger.info('load image data')
+                resource["logger"].info('load image data')
                 
                 for image_name, image_byte in image_data.items():
-                    logger.info(image_ocr.convert_img_to_txt(image_byte))
+                    try:
+                        image_type, image_text = resource["image_ocr"].convert_img_to_txt(image_byte)
+                        json_data[curr_key]['images'][image_name] = image_text
+                        resource["logger"].info(f"Image {image_type}: {image_name} processed successfully.")
+                    except Exception as e:
+                        resource["logger"].error(f"Error processing image {image_name}: {e}")
+                        # curr_json[image_name] = f"Error: {e}"
                     # curr_json[image_name] = 
 
     st.write("업로드된 JSON 데이터:")
