5bef246c5bd79f08f79973873ab6612bac38770a
차민수 <66284303+Blue-Ladder@users.noreply.github.com>
2025-04-29 08:30:13 +0900

    DOCS: 수식 추출 오류 예외 처리를 위한 Exception_FIx.py 생성

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -11,51 +11,8 @@ OUTPUT_JSON = OUTPUT_DIR / "output.json"
 # 추출할 Ctrl obj
 CTRL_TYPES = ["표", "그림", "수식"]
 
-# 유니코드 폰트 문자 → LaTeX 변환 맵
-UNICODE_LATEX_MAP = {
-    "ℤ": r"\mathbb{Z}",
-    "ℝ": r"\mathbb{R}",
-    "ℕ": r"\mathbb{N}",
-    "ℍ": r"\mathbb{H}",
-    "ℚ": r"\mathbb{Q}",
-    "ℂ": r"\mathbb{C}",
-    "ℙ": r"\mathbb{P}",
-    "not=": r"\neq"
-}
-
-# LaTex 문자 → 유니코드 폰트 변환맵
-LATEX_UNICODE_MAP = {
-    "\\text{mathbbZ}" : "ℤ",
-    "\\text{mathbbR}" : "ℝ",
-    "\\text{mathbbN}" : "ℕ",
-    "\\text{mathbbQ}" : "ℚ",
-    "\\text{mathbbC}" : "ℂ",
-    "\\text{mathbbH}" : "ℍ",
-    "\\text{mathbbP}" : "ℙ"
-}
-
 # image 분류 카테고리
-IMAGE_CATEGORY = {
-    "Graph": [
-        'a picture including curve graph',
-        'a picture including linear graph',
-        'a picture including bar graph',
-        'a picture including unexpected data',
-    ],
-
-    "Formula": [
-        'equilibrium expression for a chemical',
-        'formula including fraction or symbols',
-    ],
-    
-    "Text": [
-        'a sentence including Hangul',
-        'a phrase including Hangul',
-        'a photo including Hangul',
-        'a sentence including English word',
-        'a sentence including korean language',
-    ]
-}
+IMAGE_CATEGORY = ["Latex", "Chart", "Other"]
 
 FORMULA_OCR_MESSAGE = [
     {
